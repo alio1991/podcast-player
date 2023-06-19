bd3053a26e6e8d528fe8c8ffcbcecaa2e860873c
Alio <alioagudo@gmail.com>
2023-06-19 15:52:32 +0200

    feature: card component improved

--- src/components/PodcastCard/PodcastCard.jsx
+++ src/components/PodcastCard/PodcastCard.jsx
@@ -1,12 +1,16 @@
+import { useEffect } from 'react';
 import './PodcastCard.scss';
   
 function PodcastCard({image, title, author}) {
 
+    const maxTitleLength = 60;
+    const maxAuthorLength = 40;
+
     return (
         <div className="podcast-card">
             <img src={image} alt="" />
-            <h1>{title}</h1>
-            <h3>{author}</h3>
+            <h2>{title.length < maxTitleLength ? title : title.slice(0, maxTitleLength) + '...'}</h2>
+            <h3>{author.length < maxAuthorLength ? author : author.slice(0, maxAuthorLength) + '...'}</h3>
         </div>
     )
 }
